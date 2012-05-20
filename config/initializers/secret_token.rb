9603a0ee0598251e832ce6ae40ecfe13ae122ff0
Charlie Gleason <hi@superhighfives.com>
2012-05-20 22:05:32 +1000

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Albumlove::Application.config.secret_token = '78d95c2891c582ac79020879dba66b933386fc34b9484ab51e64654c0c2f20cacc52ee0244a55ad863c7c3e8ae27a31650961e17c5f91bde2f9d8cbba2511fdc'
