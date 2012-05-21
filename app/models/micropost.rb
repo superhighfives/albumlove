615cc5b69a98d2800efac26051e9206fd0915a69
Charlie Gleason <hi@charliegleason.com>
2012-05-22 00:07:13 +1000

    Removed test app

--- app/models/micropost.rb
+++
@@ -1,5 +0,0 @@
-class Micropost < ActiveRecord::Base
-  attr_accessible :content, :user_id
-  belongs_to :user
-  validates :content, :length => { :maximum => 140 }
-end
--- app/models/user.rb
+++
@@ -1,4 +0,0 @@
-class User < ActiveRecord::Base
-  attr_accessible :email, :name
-  has_many :microposts
-end
