76db874fd7bb34a9cfb244a6281195d0c5d4abc4
Charlie Gleason <hi@charliegleason.com>
2012-05-22 00:28:46 +1000

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Albumlove::Application.config.secret_token = 'a2528ae18c839fdd0c53dc3915c65ea38dd53f0d786e30308523da2233f264bbbd964a49999197a5812fb60c0303fae38ead8743e6e3a901d5768f08d8844e14'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Albumlove::Application.config.session_store :cookie_store, key: '_albumlove_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Albumlove::Application.config.session_store :active_record_store
