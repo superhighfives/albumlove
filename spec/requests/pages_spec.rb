fabaa4640c605f4d63ff2bb7d82da879ca6fdc80
Charlie Gleason <hi@charliegleason.com>
2012-05-28 23:26:12 +1000

    - Added title helper

--- spec/requests/pages_spec.rb
+++ spec/requests/pages_spec.rb
@@ -8,9 +8,18 @@ describe "Pages" do
       visit '/pages/home'
       page.should have_content('Sample App')
     end
-    it "should have the right title" do
+     it "should have the h1 'Sample App'" do
       visit '/pages/home'
-      page.should have_selector('title', :text => "| Home")
+      page.should have_selector('h1', :text => 'Sample App')
+    end
+    it "should have the base title" do
+      visit '/pages/home'
+      page.should have_selector('title',
+      :text => "Albumlove")
+    end
+    it "should not have a custom page title" do
+      visit '/pages/home'
+      page.should_not have_selector('title', :text => '| Home')
     end
   end
 
