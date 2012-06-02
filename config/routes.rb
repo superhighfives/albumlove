f064c83fdb7598f827e47b44ffc82c1a7f003fdd
Charlie Gleason <hi@charliegleason.com>
2012-06-03 01:44:34 +1000

    - Added sign in and sign out

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,12 @@
 Albumlove::Application.routes.draw do
   resources :users
+  resources :sessions, only: [:new, :create, :destroy]
 
   root to: 'pages#home'
 
   match '/signup',  to: 'users#new'
+  match '/signin',  to: 'sessions#new'
+  match '/signout', to: 'sessions#destroy', via: :delete
 
   match '/about', to: 'pages#about'
   match '/help', to: 'pages#help'
