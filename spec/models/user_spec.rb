c187aec05ffed1152d7a6af62c9ac15d965c6fc4
Charlie Gleason <hi@charliegleason.com>
2012-06-02 23:34:06 +1000

    - Updated validation for mixed case emails

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -60,6 +60,15 @@ describe User do
       end      
     end
   end
+  describe "email address with mixed case" do
+    let(:mixed_case_email) { "Foo@ExAMPle.CoM" }
+
+    it "should be saved as all lower-case" do
+      @user.email = mixed_case_email
+      @user.save
+      @user.reload.email.should == mixed_case_email.downcase
+    end
+  end
   describe "when email address is already taken" do
     before do
       user_with_same_email = @user.dup
